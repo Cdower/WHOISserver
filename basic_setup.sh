c31a3896a809b3c72bd5de3139e643b05489b6f1
Chris Dower <cliffjumpers57@gmail.com>
2016-09-26 19:54:50 -0700

    print respses

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,4 +1,4 @@
-#-i  all sudo? run script as sudo?
+#-i  all sudo? run script as sudo/root?
 
 yum install epel-release.noarch
 yum -y install mariadb-server mariadb
@@ -9,4 +9,8 @@ systemctl start mariadb.service
 #setup password and stuff ignore for now
 #mysql_secure_installation
 
+#Yum repo
+yum install epel-release yum-plugin-priorities
+curl -o /etc/yum.repos.d/powerdns-auth-40.repo https://repo.powerdns.com/repo-files/centos-auth-40.repo
+#install pdns and backend
 yum -y install pdns pdns-backend-mysql
--- whois_server.py
+++ whois_server.py
@@ -11,21 +11,37 @@ host = config.get('whois_server', 'host')
 port = int(config.get('whois_server', 'port'))
 import SocketServer
 
+import IPy
+
 class WhoisHandler(SocketServer.BaseRequestHandler):
     """
     Handles whois requests
     """
     def handle(self):
         syslog.syslog(syslog.LOG_INFO, self.client_address[0] + ' is connected') #log client connections
+
+        queries = 0
         while 1:
             query = self.request.recv(1024).strip()
             if query == '':
+                #log number of queries  from client
                 break
+            ip = None
             print self.client_address[0] + ': ' + query
+            queries += 1
+            try:
+                ip = IPy.IP(query)
+            except:
+                pass
+            if ip:
+                #response = query_ip(ip)
+                response = 'response: ' + ip
+            else:
+                #response = query_url(query)
+                response = 'response: ' + query
+            self.request.send(response + '\n\n')
         #log self.client_address[0] #log to redis?
         #setup query
-
-
 
 server = SocketServer.ThreadingTCPServer((host, port), WhoisHandler)
 server.serve_forever()
